ae616b0f244e548098606ffe0c74928d7bca7608
MattWhittle <themwproject@yahoo.com>
2017-09-13 12:48:58 -0400

    updating to xy-grid

--- app/components/Projects/ProjectsSlideInRight.jsx
+++ app/components/Projects/ProjectsSlideInRight.jsx
@@ -3,11 +3,9 @@ const React = require('react');
 var HomeSlideInRight = React.createClass({
     render: function() {
             return (
-                <div className="row">
-                    <div className="small-centered medium-6">
-                    <div className="medium-6 column slideInCopy">COLUMN</div>
-                    <div className="medium-6 column"><img src="/images/slideIn1.jpg" /></div>
-                    </div>
+                <div className="grid-x projects-content">
+                    <div className="medium-6 cell slideInCopy">cell</div>
+                    <div className="medium-6 cell"><img src="/images/slideIn1.jpg" /></div>
                 </div> 
             )
     }
