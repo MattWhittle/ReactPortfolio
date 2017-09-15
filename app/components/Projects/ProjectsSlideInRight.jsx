1a317d4bf11dc0461c2dfa9d04ddbe4d638e16fe
MattWhittle <themwproject@yahoo.com>
2017-09-15 17:49:13 -0400

    added styling to nav

--- app/components/Projects/ProjectsSlideInRight.jsx
+++ app/components/Projects/ProjectsSlideInRight.jsx
@@ -4,7 +4,7 @@ var HomeSlideInRight = React.createClass({
     render: function() {
             return (
                 <div className="grid-x projects-content">
-                    <div className="medium-6 cell slideInCopy">cell</div>
+                    <div className="medium-6 cell slideInCopy"><h1>cell</h1></div>
                     <div className="medium-6 cell"><img src="/images/slideIn1.jpg" /></div>
                 </div> 
             )
