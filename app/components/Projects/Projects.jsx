800435c35d8a66d3e99cb19c660087303ad18e00
MattWhittle <themwproject@yahoo.com>
2017-10-12 06:11:49 -0400

    DRY clean up on projects

--- app/components/Projects/Projects.jsx
+++ app/components/Projects/Projects.jsx
@@ -3,15 +3,64 @@ const ProjectsSlideInRight = require('app/components/Projects/ProjectsSlideInRig
 
 
 var Projects = React.createClass({
+    getInitialState: function() {
+        return {
+            projects: [{
+                id: 1,
+                title: "Search App",
+                description: "React app",
+                livelink: "http://cryptic-escarpment-56760.herokuapp.com/",
+                githublink: "https://github.com/MattWhittle/ReactAssociateSearch",
+                imagesource: "/images/searchApp.jpg"
+            },
+            {
+                id: 2,
+                title: "Travel blog",
+                description: "Wordpress Theme",
+                githublink: "https://github.com/MattWhittle/travel-blog-theme",
+                imagesource: "/images/travelBlog.jpg"
+            },
+            {
+                id: 3,
+                title: "Wedding Site",
+                description: "HTML & CSS",
+                livelink: "https://mattwhittle.github.io/wedding-firstsite/",
+                githublink: "https://github.com/MattWhittle/wedding-firstsite",
+                imagesource: "/images/weddingSite.jpg"
+            },
+            {
+                id: 4,
+                title: "Weather App", 
+                description: "React app",
+                livelink: "http://fierce-citadel-63831.herokuapp.com/#/?_k=h8jeuk",
+                githublink: "https://github.com/MattWhittle/reactweather",
+                imagesource: "/images/weatherApp.jpg"
+            },
+            {
+                id: 5,
+                title: "Timer App",
+                description: "React app",
+                livelink: "http://thawing-waters-34194.herokuapp.com/#/?_k=nc1qg6",
+                githublink: "https://github.com/MattWhittle/ReactTimer",
+                imagesource: "/images/timerApp.jpg" 
+            }
+            ]
+        }
+    },
     render: function() {
+            let {projects} = this.state;
+
+            const renderprojects = ()=> {
+                return projects.map((project) => {
+                    return (
+                        <ProjectsSlideInRight key={project.id} {...project} />
+                    );
+                });
+            };
             return (
                 <div>
                    <h1 className="page-headline">Projects</h1>
-                   <ProjectsSlideInRight title="Search App" description="React app" livelink="http://cryptic-escarpment-56760.herokuapp.com/" githublink="https://github.com/MattWhittle/ReactAssociateSearch" imagesource="/images/searchApp.jpg" />
-                   <ProjectsSlideInRight title="Travel blog" description="Wordpress Theme" githublink="https://github.com/MattWhittle/travel-blog-theme" imagesource="/images/travelBlog.jpg" />                   
-                   <ProjectsSlideInRight title="Wedding Site" description="HTML & CSS" livelink="https://mattwhittle.github.io/wedding-firstsite/" githublink="https://github.com/MattWhittle/wedding-firstsite" imagesource="/images/weddingSite.jpg" />
-                   <ProjectsSlideInRight title="Weather App" description="React app" livelink="http://fierce-citadel-63831.herokuapp.com/#/?_k=h8jeuk" githublink="https://github.com/MattWhittle/reactweather" imagesource="/images/weatherApp.jpg" />
-                   <ProjectsSlideInRight title="Timer App" description="React app" livelink="http://thawing-waters-34194.herokuapp.com/#/?_k=nc1qg6" githublink="https://github.com/MattWhittle/ReactTimer" imagesource="/images/timerApp.jpg" />
+                   {renderprojects()}
                 </div> 
             )
     }
