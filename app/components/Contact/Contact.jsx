14cb8f261e6d6eacdcd3ec70ed8f5f74ad2f8880
MattWhittle <themwproject@yahoo.com>
2017-10-11 21:42:17 -0400

    updated links to target new tab

--- app/components/Contact/Contact.jsx
+++ app/components/Contact/Contact.jsx
@@ -7,9 +7,9 @@ var Contact = React.createClass({
                 <div className="contact-page">
                    <h1 className="page-headline">Contact</h1>
                    <div className="contact-info">
-                    <a href="https://github.com/mattwhittle"><i className="fi-social-github social-icon">  View My Github</i></a><br />
-                    <a href="https://www.linkedin.com/in/matt-whittle/"><i className="fi-social-linkedin social-icon">  View My LinkedIn</i></a><br />
-                    <a href="mailto:matt.v.whittle@gmail.com"><i className="fi-mail social-icon">  Email Me</i></a><br />
+                    <a href="https://github.com/mattwhittle" target="_blank"><i className="fi-social-github social-icon">  View My Github</i></a><br />
+                    <a href="https://www.linkedin.com/in/matt-whittle/" target="_blank"><i className="fi-social-linkedin social-icon">  View My LinkedIn</i></a><br />
+                    <a href="mailto:matt.v.whittle@gmail.com" target="_blank"><i className="fi-mail social-icon">  Email Me</i></a><br />
                    </div>
                 </div> 
             )
